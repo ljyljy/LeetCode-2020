d593540894b6fd768af5f9a0ed10e139cd1b9f73
ljyljy <ljyljyok@vip.qq.com>
2020-09-27 11:01:43 +0800

    2020.9.27

---
+++ Array/q9_405-submatrix-sum.py
@@ -0,0 +1,87 @@
+# 	Q9_405. 和为0的子矩阵
+# 给定一个整数矩阵，请找出一个子矩阵，使得其数字之和等于0.输出答案时，请返回左上数字和右下数字的坐标。如果有多个答案, 你可以返回其中任意一个.
+# 说明：
+# 0 ≤ N ≤ 30
+# 样例1：
+# 输入:
+# [
+#   [1, 5, 7],
+#   [3, 7, -8],
+#   [4, -8 ,9]
+# ]
+# 输出: [[1, 1], [2, 2]]
+# 样例 2:
+# 输入:
+# [
+#   [0, 1],
+#   [1, 0]
+# ]
+# 输出: [[0, 0], [0, 0]]
+# 挑战： O(n3) 时间复杂度。
+# 相关题目： 944. 最大子矩阵139. 最接近零的子数组和138. 子数组之和
+# 用前缀和优化, 令 sum[i][j] = sum[0][j] + sum[1][j] + ... + sum[i][j]
+# 然后枚举上下边界, 这样就相当于在一行内, 求一个数组连续子串和为0的问题了.
+#
+# 算法思路：
+# •	求出数组的前缀和数组pre，对于左上角(x1,y1)，右下角(x2,y2)的矩阵，矩阵和为pre[x2][y2]-pre[x1-1][y2]-pre[x2][y1-1]+pre[x1-1][y1-1]
+# •	那么我们枚举左上角和右小角，找到其中一个和为0的矩阵即可
+# 代码思路
+# 1.	求出数组的前缀和数组pre
+# 2.	枚举左上角位置
+# 3.	对于每个左上角，在其位置的右下部分枚举右下角
+# 4.	计算子矩阵和，若为0则返回
+# 复杂度分析： N表示数组长度
+# •	空间复杂度：O(N2)
+# •	时间复杂度：O(N4)
+
+class Solution:
+    # 法2：空间O(n^3) - hash
+    def submatrixSum2(self, matrix):
+        if not matrix or not matrix[0]: return None
+        nrow, ncol = len(matrix), len(matrix[0])
+
+        # 枚举上下边界nrow（pair<top_row, bottom_row> -- 某两行）
+        for top_row in range(nrow):
+            tmp_row = [0] * ncol
+            for bottom_row in range(top_row, nrow):
+                prefix_hash = {0: -1}
+                # 求出固定上下边界的前缀和
+                prefix_sum = 0
+                for col in range(ncol):
+                    tmp_row[col] += matrix[bottom_row][col]
+                    prefix_sum += tmp_row[col]
+                    # hashmap判断是否存在相同前缀和
+                    if prefix_sum in prefix_hash:  # ↓ 记得要+1！！！
+                        return [(top_row, prefix_hash[prefix_sum] + 1), (bottom_row, col)]
+                    prefix_hash[prefix_sum] = col
+        return None
+
+    # 法1：空间O(n^4)
+    def submatrixSum(self, matrix):
+        nrow, ncol = len(matrix), len(matrix[0])
+        res = []
+        # 1. 求出前缀和数组   # 边界 pre[0,:] = pre[:, 0] = 0
+        pre = [[0 for _ in range(ncol + 1)] for _ in range(nrow + 1)]
+        pre[1][1] = matrix[0][0]  # 1.1 前缀和[1][1] <=> 对应matrix[0][0]
+        # 1.2 初始化前缀和的第1列(2:n, 1)
+        for i in range(2, nrow + 1):
+            pre[i][1] = pre[i - 1][1] + matrix[i - 1][0]  # pre[i][1]对应位置的matrix下标为[i-1, 0]
+        # 1.3 初始化前缀和的第1行(1, 2:j)
+        for j in range(2, ncol + 1):
+            pre[1][j] = pre[1][j - 1] + matrix[0][j - 1]  # pre[1][j]对应位置的matrix下标为[0, j-1]
+        # 1.4 填充[2:nrow, 2:ncol]
+        for i in range(2, nrow + 1):
+            for j in range(2, ncol + 1):
+                pre[i][j] = pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1] + matrix[i - 1][j - 1]
+
+        # 枚举左上角
+        for x1 in range(1, nrow + 1):
+            for y1 in range(1, ncol + 1):
+                # 枚举右下角
+                for x2 in range(x1, nrow + 1):
+                    for y2 in range(y1, ncol + 1):
+                        # 若矩阵和为0，则返回 ❤❤❤ ↓容易出错！！
+                        if pre[x2][y2] - pre[x1 - 1][y2] - pre[x2][y1 - 1] + pre[x1 - 1][y1 - 1] == 0:
+                            res.append([x1 - 1, y1 - 1])
+                            res.append([x2 - 1, y2 - 1])
+                            return res
---
+++ DataStructure/SegmentTree/q202-segment-tree-query.py
@@ -0,0 +1,67 @@
+# 	Q202. 线段树的查询 - segment-tree-query
+# 对于一个有n个数的整数数组，在对应的线段树中, 根节点所代表的区间为0-n-1, 每个节点有一个额外的属性max，值为该节点所代表的数组区间start到end内的最大值。
+# 为SegmentTree设计一个 query 的方法，接受3个参数root, start和end，线段树root所代表的数组中子区间[start, end]内的最大值。
+# 说明: 线段树(又称区间树), 是一种高级数据结构，他可以支持这样的一些操作:
+# 查找给定的点包含在了哪些区间内
+# 查找给定的区间包含了哪些点
+# 见百科：线段树、区间树
+# 样例 1:
+# 输入："[0,3,max=4][0,1,max=4][2,3,max=3][0,0,max=1][1,1,max=4]
+# [2,2,max=2][3,3,max=3]",1,2
+# 输出：4
+# 解释：对于数组 [1, 4, 2, 3], 对应的线段树为 :
+# 	                  [0, 3, max=4]
+# 	                 /             \
+# 	          [0,1,max=4]        [2,3,max=3]
+# 	          /         \        /         \
+# 	   [0,0,max=1] [1,1,max=4] [2,2,max=2], [3,3,max=3]
+# [1,2]区间最大值为4
+# 样例 2:
+# 输入："[0,3,max=4][0,1,max=4][2,3,max=3][0,0,max=1][1,1,max=4][2,2,max=2][3,3,max=3]",2,3
+# 输出：3
+# 解释： 对于数组 [1, 4, 2, 3], 对应的线段树为 :
+# 	                  [0, 3, max=4]
+# 	                 /             \
+# 	          [0,1,max=4]        [2,3,max=3]
+# 	          /         \        /         \
+# 	   [0,0,max=1] [1,1,max=4] [2,2,max=2], [3,3,max=3]
+# [2,3]区间最大值为3
+# 相关题目:  833. 进程序列751. 约翰的生意247. 线段树查询 II203. 线段树的修改
+
+import sys
+
+
+class SegmentTreeNode:
+    def __init__(self, start, end, max):
+        self.start, self.end = start, end
+        self.max = max
+        self.left, self.right = None, None
+
+
+class Solution:
+    def query(self, root, start, end):
+        if start <= root.start and end >= root.end:
+            return root.max
+
+        overlap = lambda node: max(start, node.start) <= min(end, node.end)
+
+        return max(self.query(node, start, end) for node in (root.left, root.right)
+                   if node and overlap(node))
+
+    def query1(self, root, start, end):
+        if start == root.start and end == root.end:
+            return root.max
+        mid = root.start + root.end >> 1
+        max_left = -sys.maxsize
+        max_right = -sys.maxsize
+        if start <= mid:
+            if mid < end:
+                max_left = self.query(root.left, start, mid)
+            else:
+                max_left = self.query(root.left, start, end)
+        if mid < end:
+            if start <= mid:
+                max_right = self.query(root.right, mid + 1, end)
+            else:
+                max_right = self.query(root.right, start, end)
+        return max(max_right, max_left)
