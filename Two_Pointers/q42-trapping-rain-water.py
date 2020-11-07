bf3d42f3882b83dbf5dc6ef26c3ca8c58a4c4c45
ljyljy <ljyljyok@vip.qq.com>
2020-11-07 09:46:44 +0800

    2020.11.07

--- Two_Pointers/q42-trapping-rain-water.py
+++ Two_Pointers/q42-trapping-rain-water.py
@@ -40,7 +40,7 @@ class Solution:
         return water
 
     # 法2：双向指针 -- 时间O(n), 空间O(1)
-    def trap(self, heights: List[int]) -> int:
+    def trap2(self, heights: List[int]) -> int:
         if not heights: return 0
         left, right = 0, len(heights) - 1
         left_max, right_max = heights[left], heights[right]
@@ -54,4 +54,19 @@ class Solution:
                 right_max = max(right_max, heights[right])
                 water += right_max - heights[right]  # 当前右指针列 计算完毕
                 right -= 1
+        return water
+
+    # 法3 – 单调栈：时间复杂度：O(n)，空间复杂度 O(n)
+    # ❤ 和双指针法不同，单调栈不需要全局信息，输入是数据流的情况也可以处理。缺点是最坏O(N)空间。但你要是只会双指针，面试官测你数据流，你就抓瞎了。
+    def trap(self, heights):
+        stack, water = [], 0
+        for i, cur_h in enumerate(heights):
+            while stack and cur_h > heights[stack[-1]]:  # cur_h > 栈顶h，可以接水！
+                left_min1_h = heights[stack.pop()]  # 左侧/栈顶h比cur_h矮。将被'次栈顶' & cur_h淹没的'当前栈顶'pop，后面用不到了（此后可能会用到次栈顶）
+                if not stack: continue  # ❤ 不可少！
+                left_min2_idx = stack[-1]
+                min_h = min(heights[left_min2_idx], cur_h)  # ‘(原)次栈顶’ vs 当前高度
+                width, h = i - left_min2_idx - 1, min_h - left_min1_h
+                water += h * width
+            stack.append(i)  # stack为空 || cur_h < 栈顶h，无法接水，暂时进栈
         return water
