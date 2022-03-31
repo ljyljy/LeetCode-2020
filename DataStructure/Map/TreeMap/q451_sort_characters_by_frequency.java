c0e415e9e2a675350d06fb04ab08836fc955b3a6
ljyljy <ljyljyok@vip.qq.com>
2022-03-31 21:10:08 +0800

    2022/3/31

---
+++ DataStructure/Map/TreeMap/q451_sort_characters_by_frequency.java
@@ -0,0 +1,26 @@
+package DataStructure.Map.TreeMap;
+
+import javafx.scene.layout.Priority;
+
+import java.util.*;
+
+public class q451_sort_characters_by_frequency {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>(); // ´ÊÆµ½µÐò
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Character, Integer>> entryMaxHeap =
+                new PriorityQueue<>((o1, o2) -> (o2.getValue() - o1.getValue()));
+        entryMaxHeap.addAll(map.entrySet());
+
+        StringBuilder sb = new StringBuilder();
+        while (!entryMaxHeap.isEmpty()) {
+            Map.Entry<Character, Integer> entry = entryMaxHeap.poll();
+            sb.append((entry.getKey()+"").repeat(entry.getValue()));
+        }
+        return sb.toString();
+
+    }
+}
