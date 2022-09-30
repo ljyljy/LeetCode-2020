f8f673ae76e6ce8b831439f8ce97d46ca6cd6f0d
ljyljy <ljyljyok@vip.qq.com>
2022-09-30 11:47:05 +0800

    2022/09/30

--- BFS/ShortestPath/q9_1565_modern_ludo_i_FacA.java
+++ BFS/ShortestPath/q9_1565_modern_ludo_i_FacA.java
@@ -2,10 +2,11 @@ package BFS.ShortestPath;
 
 import java.util.*;
 
-// A厂（Amazon）真题
+// A厂（Amazon）真题，4种解法，思路、优化见 q9_1565_modern_ludo_i_SPFA_FacA.java
+
 public class q9_1565_modern_ludo_i_FacA {
     // 法1：BFS & 简易 SPFA(最短路径快速算法)
-    //      优化见 q9_1565_modern_ludo_i_SPFA_FacA.java
+    //      优化见q9_1565_modern_ludo_i_SPFA_FacA.java
     public int modernLudo_1(int len, int[][] conns) {
         int minDist = 0;
         Deque<Integer> deque = new ArrayDeque<>(); // todo：优化为PQ
@@ -30,7 +31,7 @@ public class q9_1565_modern_ludo_i_FacA {
                 int start = conn[0], end = conn[1];
                 if (curIdx == start && dists[curIdx] < dists[end]) {
                     deque.offer(end);
-                    dists[end] = dists[curIdx]; // // 更新[直连]通路, [距离不变]
+                    dists[end] = dists[curIdx]; // 更新[直连]通路, [距离不变]
                 }
             }
         }
@@ -41,6 +42,7 @@ public class q9_1565_modern_ludo_i_FacA {
 
     // 法0：BFS+BFS
     //      外层 BFS 做最短路径，内层 BFS 找连通块。
+    //       内层BFS【强行将下层结点中，距离与本层一样的结点，合并入本层】
     Map<Integer, Set<Integer>> graph = new HashMap<>(); // <idx, 领接表>
     Map<Integer, Integer> distMap = new HashMap<>(); // <idx, minDist>
     
@@ -57,7 +59,8 @@ public class q9_1565_modern_ludo_i_FacA {
                 // 复杂图：路径越长，可能dist不递增
                 //   如：C-E直通，则BFS从C下探到E时，层数++，但距离不变
                 //      违反BFS-简单图下，层数↑，距离↑的规律！
-                //      因此需要内层BF S(或SPFA, 下探时更新最短路)，将本层连通的所有点找到
+                //      因此需要内层BFS(或SPFA, 下探时更新最短路; 或两队列交替)，将本层连通的所有点找到
+                //      内层BFS:【强行将下层结点中，距离与本层一样的结点，合并入本层】
                 //          如：[2,5], [5,8], [8,10]: 2与5、8、10都是直连，距离为0！！！
                 Set<Integer> nxtNodes = getNxtUnvisited(neigh); // BFS-2
                 for (int nxt: nxtNodes) {
@@ -100,42 +103,41 @@ public class q9_1565_modern_ludo_i_FacA {
     }
 
 
-    // todo：法2 - dp
+    // [最优解]：法2 - dp
     // dp： 坑有点多，因为存在connections的两地s -> t的目的地t对应的s可能是多个的
     //  而且，多个s中不能使用贪心算法直接取最小的s来得到最短步数（换句话说，就是你跳跃的越远不见得能减少步数）
     //  所以这里用一个dic来存t对应的s值，有多个s值的就放在list里面。
     // 转移方程：
     //      没有connections时： dpi = min(dpi, dpi-j + 1) 其中i - j > 0, j为1-6的骰子点数
     //      有connections时： dpi = min(dpi, dpk) k -> i对应的连接起始点的位置，有多个时，遍历取min(dpk)
-    public int modernLudo_2(int length, int[][] connections) {
-        // Write your code here
-        if (length <= 7) {
-            return 1;
-        }
-        int []f = new int[length + 1];
-        int []dp = new int[length + 1];
-        for (int i = 1; i <= length; ++i) {
-            f[i] = i;
+
+    //    Map<Integer, Set<Integer>> graph = new HashMap<>();
+    public int modernLudo_dp(int len, int[][] conns) {
+        buildGraph(len, conns);
+        int[] dp = new int[len + 1]; // 下标idx∈[1, len]
+        for (int i = 1; i <= len; i++) {
             dp[i] = Integer.MAX_VALUE;
         }
-        dp[1] = 0;
-        for (int i = 0; i < connections.length; ++i) {
-            f[connections[i][0]] = connections[i][1];
-        }
-        for (int i = 2; i <= length; ++i) {
-            if (i - 6 < 1) {
-                dp[i] = 1;
-            } else {
-                for (int j = i-1; j > i-7; j--) {
-                    dp[i] = Math.min(dp[j]+1, dp[i]);
+        dp[len] = 0; // 终点，从后往前倒推
+        for (int cur = len-1; cur >= 1; cur--) {
+            for (int nxt = cur+1; nxt <= cur+6 && nxt <= len; nxt++) {// 遍历1）骰子点数∈[1, 6]
+                if (dp[nxt] == Integer.MAX_VALUE) {
+                    continue; // 防止下面dp[nxt]+1溢出
                 }
+                dp[cur] = Math.min(dp[cur], dp[nxt]+1);
             }
-            dp[f[i]] = Math.min(dp[i], dp[f[i]]);
+            for (int nxt: graph.get(cur)) { // 遍历2）连通域
+                dp[cur] = Math.min(dp[cur], dp[nxt]); // 更新[直连]通路, [距离不变]
+            }
         }
-        return dp[length];
+        return dp[1];
     }
 
     // todo: 法3 - 两个队列交替
+    //	使用 BFS 实现中两个队列交替的方法
+    //	将通过掷骰子跳到的点放在下一个队列里
+    //	将通过直连到达的点放在当前队列里
+    //	这样就不会破坏每个队列为同一层节点的属性
     public int modernLudo_3(int length, int[][] connections) {
         buildGraph(length, connections);
 
--- BFS/ShortestPath/q9_1565_modern_ludo_i_SPFA_FacA.java
+++ BFS/ShortestPath/q9_1565_modern_ludo_i_SPFA_FacA.java
@@ -2,6 +2,64 @@ package BFS.ShortestPath;
 
 import java.util.*;
 
+/**
+ 思路：
+     如何找到一步能跳到的所有点？—— BFS!
+     ?	将直通关系看做图中的边
+     ?	所有和 2 连通的点都是可以通过 2 一步跳到的【内层BFS】
+     ?	找连通块问题就是 BFS 的功能
+
+     方法2： BFS+BFS
+     ?	外层 BFS 做最短路径
+     ?	内层 BFS 找连通块
+         ?	将直通关系 connections 构图（邻接表）
+             ?	Python: dict{node: set(neighbors)}
+             ?	Java: HashMap<Node, HashSet<Node>>
+         ?	邻接表 (Adjacency List)
+             ?	本意是用 List 存储 neighbors 的
+             ?	但是实战中用 Set 还可以去重和 O(1) 查询 a, b 两个点是否有连边
+
+     方法3： 两个队列交替（todo）
+     ?	使用 BFS 实现中两个队列交替的方法
+     ?	将通过掷骰子跳到的点放在下一个队列里
+     ?	将通过直连到达的点放在当前队列里
+     ?	这样就不会破坏每个队列为同一层节点的属性
+         ?	意思是：BFS-简单图下，层数↑，距离↑的规律！
+         ?	解决：
+             ?	外层BFS + 内层BFS【强行将下层结点中，距离与本层一样的结点，合并入本层】
+             ?	SPFA, 下探时更新最短路，将本层连通的所有点找到
+             ?	两个队列交替
+
+     方法3： SPFA
+     ?	SPFA = Shortest Path Fast Algorithm
+     ?	这个算法实际上是基于 BFS 算法的一个拓展
+     ?	基础BFS 算法求最短路要求简单图
+     ?	SPFA 可以解决非简单图的最短路径
+     ?	问： 为什么 BFS 不能解决复杂图？
+         ?	简单图中： 第几层访问到该节点=到达该节点的最短路径
+         ?	复杂图中： 可能通过三层访问到比通过两层访问到的路径更短
+
+     ?	SPFA 的解决方式复杂图中访问层级和最短路径不匹配的办法是:
+         ?	如果我在第三层中发现一个第二层中访问过的节点
+         ?	但是此时找到的路径更短
+         ?	就丢回队列——再从这个节点出发往下拓展
+     ?	SPFA: 一个节点是否被扔进队列的判断标准发生变化：
+         ?	简单图： 没有访问过的点就扔进队列
+         ?	复杂图： 如果到达该点的路径变短了就扔进队列
+             ?	优化：SPFA 里会使用 Heap(PriorityQueue) 来替换 Queue，这样能够更快的找到最短路径（类比：A*、Dijkstra算法）
+
+     方法4： 动态规划(DP)
+     ?	找最短路径也在动态规划的魔爪范围内
+     ?	由于规定了跳跃的方向性（从左到右）
+     ?	可以使用坐标型动态规划自底向上（从右至左） 来计算
+         ?	state: dp[i] 表示从 i 出发跳到终点需要最少多少步
+         ?	function:
+             ?	dp[i] = min(dp[i], dp[j] + 1(by投骰子) || dp[j](by连通域))
+         ?	j 是 i 可以跳到(骰子)或者直通的点, 跳过去的话步数+1， 直通的话步数 + 0
+         ?	initialization: dp[1..n-1] = INT_MAX, dp[n] = 0
+         ?	answer: dp[1]
+
+ **/
 public class q9_1565_modern_ludo_i_SPFA_FacA {
     // 【荐，避免两层BFS！】 法1：SPFA(最短路径快速算法)
     Map<Integer, Set<Integer>> graph = new HashMap<>();
