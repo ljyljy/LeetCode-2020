4cc7265231e57cbc4d4b112b38f0421e08e43915
ljyljy <ljyljyok@vip.qq.com>
2023-04-05 15:57:22 +0800

    2023/04/05

--- DataStructure/Deque/q20.c
+++ DataStructure/Deque/q20.c
@@ -5,27 +5,31 @@
 #include <string.h>
 #include <stdbool.h>
 
-// 写法2：top=-1, 则入栈++top，栈顶为top，空为top=-1
-bool isValid(char* s) {
-    int n = strlen(s);
-    int stk[n + 1], top = -1;
-    for (int i = 0; i < n; i++) {
-        char c = s[i];
-        if (c == '(') stk[++top] = ')';
-        else if (c == '[') stk[++top] = ']';
-        else if (c == '{') stk[++top] = '}';
-        else if (top == -1 || c != stk[top--]) {
-            return false;
-        }
-    }
-    return top == -1; // deque.isEmpty()
-}
-
-// 写法1：top=1（不推荐）
+// 写法1：top=0（推荐）
+// top = 0, 若入栈top++，则栈顶为top-1, 取栈顶并弹栈：stk[--top]，空为top = 0
 bool isValid1(char* s) {
     int n = strlen(s);
     char* stk = (char*)malloc(sizeof(char) * n);
+    int top = 0;// top：始终是【待填入】栈顶的idx
+    for (int i = 0; i < n; i++) {
+        char c = s[i];
+        if (c == '(') stk[top++] = ')';
+        else if (c == '[') stk[top++] = ']';
+        else if (c == '{') stk[top++] = '}';
+        else if (top == 0 || c != stk[--top]) {
+            return false;
+        }
+    }
+    return top == 0; // 【勿忘】deque.isEmpty()
+}
+
+// 写法1-2：top=0（推荐）
+// top = 0, 若入栈top++，则栈顶为top-1, 取栈顶并弹栈：stk[--top]，空为top = 0
+bool isValid(char* s) {
+    int n = strlen(s);
+    int stk[n]; // char* stk可以转化为int stk[]或 char stk[]
     int top = 0;
+
     for (int i = 0; i < n; i++) {
         char c = s[i];
         if (c == '(') stk[top++] = ')';
@@ -36,6 +40,43 @@ bool isValid1(char* s) {
         }
     }
     return top == 0;
+}
+
+// 写法2-1：top=-1, 若入栈++top，则栈顶为top, 取栈顶并弹栈：stk[top--]，空为top=-1
+bool isValid(char* s) {
+    int n = strlen(s);
+    char* stk = (char*)calloc(n, sizeof(char));
+    int top = -1;
+
+    for (int i = 0; i < n; i++) {
+        char c = s[i];
+        if (c == '(') stk[++top] = ')';
+        else if (c == '[') stk[++top] = ']';
+        else if (c == '{') stk[++top] = '}';
+        else if (top == -1 || c != stk[top--]) {
+            return false;
+        }
+    }
+    return top == -1;
+}
+
+// 写法2-2：top=-1, 若入栈++top，则栈顶为top, 取栈顶并弹栈：stk[top--]，空为top=-1
+bool isValid(char* s) {
+    int n = strlen(s);
+    char stk[n]; // char* stk可以转化为int stk[]或 char stk[]
+    int top = -1;
+
+    // 以下与 法2-1 一样
+    for (int i = 0; i < n; i++) {
+        char c = s[i];
+        if (c == '(') stk[++top] = ')';
+        else if (c == '[') stk[++top] = ']';
+        else if (c == '{') stk[++top] = '}';
+        else if (top == -1 || c != stk[top--]) {
+            return false;
+        }
+    }
+    return top == -1; // deque.isEmpty()
 }
 
 int main() {
--- Two_Pointers/q18.c
+++ Two_Pointers/q18.c
@@ -15,8 +15,21 @@
 #define DIM(arr) sizeof(arr)/sizeof(*arr)
 #endif
 
+ /**
+  * 坑：5'
+     1. 回溯中的path，动态增删：记录下标pathLen
+     2. res动态扩容！ 动态malloc & realloc！
+     3. 全局变量初始化：必须在入口
+     4. long last_pair[] = { LONG_MAX, LONG_MAX }; // 用于去重
+        并与bool equals(long p1[], long p2[])配合，去重！
+     5. 其他：
+        - 传参【**列数】的理解与赋值！（*retColCnts, 等价于int* n_cols）
+        - 传参【**行数】的理解与赋值！（*returnSize, 等价于int* curCnt）
+  */
+
+
 int basicSize;
-int idx;
+int curCnt;
 
 bool equals(long p1[], long p2[]) {
     return p1[0] == p2[0] && p1[1] == p2[1];
@@ -28,15 +41,17 @@ static inline int cmp(const void* pa, const void* pb) {
 
 int** fourSum(int* nums, int n, int target, int* returnSize, int** retColCnt) {
     qsort(nums, n, sizeof(int), cmp);
-    basicSize = 8, idx = 0;
+    basicSize = 8, curCnt = 0;
     long last_pair[] = { LONG_MAX, LONG_MAX };
 
-    int** res = (int**)malloc(sizeof(int*) * basicSize); // 动态分配！
-    int* cols = (int*)malloc(sizeof(int) * basicSize); // 每行的列数（1个int），等价于*retColCnt
+    int** res = (int**)malloc(sizeof(int*) * basicSize); // 动态分配！对res**的【行空间】进行分配。
+    int* n_cols = (int*)malloc(sizeof(int) * basicSize); // 每行的列数（1个int），等价于*retColCnt
 
     for (int i = 0; i < n - 3; i++) {
         if (i - 1 >= 0 && nums[i] == nums[i - 1]) continue;
         for (int j = i + 1; j < n - 2; j++) {
+            // ↓ 不可写！否则target=8时，会漏掉[2,2,2,2]这个解！
+            // if (j-1 >= 0 && nums[j] == nums[j-1]) continue;
             long cur_pair[] = { nums[i], nums[j] };
             if (equals(last_pair, cur_pair)) continue;
             memcpy(last_pair, cur_pair, sizeof(cur_pair)); // last_pair = cur_pair 不可！
@@ -46,18 +61,18 @@ int** fourSum(int* nums, int n, int target, int* returnSize, int** retColCnt) {
             while (lf < rt) {
                 long sum = (long)nums[i] + (long)nums[j] + (long)nums[lf] + (long)nums[rt];
                 if (sum == target) {
-                    res[idx] = (int*)calloc(4, sizeof(int));
-                    res[idx][0] = nums[i], res[idx][1] = nums[j], res[idx][2] = nums[lf], res[idx][3] = nums[rt];
-                    cols[idx] = 4; // 第idx行的列数=4，即(*retColCnt)[idx]=4;
-                    idx++;
+                    res[curCnt] = (int*)calloc(4, sizeof(int)); // 【勿忘】，【列空间】继续分配
+                    res[curCnt][0] = nums[i], res[curCnt][1] = nums[j], res[curCnt][2] = nums[lf], res[curCnt][3] = nums[rt];
+                    n_cols[curCnt] = 4; // 第idx行的列数=4，即(*retColCnt)[idx]=4;
+                    curCnt++;
                     while (lf < rt && nums[lf] == nums[lf + 1]) lf++;
                     while (lf < rt && nums[rt] == nums[rt - 1]) rt--;
                     lf++; rt--;
 
-                    if (idx == basicSize) { // 动态扩容！
+                    if (curCnt == basicSize) { // 动态扩容！
                         basicSize *= 2;
                         res = (int**)realloc(res, sizeof(int*) * basicSize);
-                        cols = (int*)realloc(cols, sizeof(int) * basicSize);
+                        n_cols = (int*)realloc(n_cols, sizeof(int) * basicSize);
                     }
                 }
                 else if (sum < target) lf++;
@@ -65,8 +80,8 @@ int** fourSum(int* nums, int n, int target, int* returnSize, int** retColCnt) {
             }
         }
     }
-    *returnSize = idx;
-    *retColCnt = cols;
+    *returnSize = curCnt;
+    *retColCnt = n_cols;
     return res;
 }
 
